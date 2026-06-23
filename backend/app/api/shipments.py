bd693afab57d1d828950636b8b5e30d8c07c6964
Mike Segana <mikeseg44@gmail.com>
2026-06-23 11:32:02 +0100

    Added start and end trip functions and endpoints

--- backend/app/api/shipments.py
+++ backend/app/api/shipments.py
@@ -1,7 +1,8 @@
 from fastapi import APIRouter
+from pydantic import BaseModel
 from app.dependencies.db import db_dependency, user_dependency
 from app.schemas.shipment import ShipmentCreate
-from app.services.shipment_service import create_shipment, get_user_shipments, get_all_shipments
+from app.services.shipment_service import create_shipment, get_user_shipments, get_all_shipments, start_transit, end_transit
 
 router = APIRouter(prefix="/shipments", tags=["shipments"])
 
@@ -18,3 +19,11 @@ def my_shipments(db: db_dependency, user:user_dependency):
 @router.get("/")
 def all_shipments(db: db_dependency, user: user_dependency):
     return get_all_shipments(db)
+
+@router.patch("/{shipment_id}")
+def start_trip(db: db_dependency, user: user_dependency, shipment_id: int):
+    return start_transit(db, shipment_id)
+
+@router.patch("/{shipment_id}/end-trip")
+def end_trip(db: db_dependency, user: user_dependency, shipment_id: int):
+    return end_transit(db, shipment_id)
--- backend/app/services/shipment_service.py
+++ backend/app/services/shipment_service.py
@@ -1,5 +1,7 @@
 from sqlalchemy.orm import Session
 from app.models.shipment import Shipment, Status
+from app.models.driver import Driver, Availability
+from fastapi import HTTPException, status
 
 def create_shipment(db: Session, user_id: int, data):
     shipment = Shipment(
@@ -19,3 +21,71 @@ def get_user_shipments(db: Session, user_id: int):
 
 def get_all_shipments(db: Session):
     return db.query(Shipment).all()
+
+def start_transit(db: Session, shipment_id: int):
+    shipment = db.query(Shipment).filter(Shipment.id == shipment_id).first()
+    if not shipment:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Shipment not found"
+        )
+    if shipment.status != Status.ASSIGNED:
+        raise HTTPException(
+            status_code=status.HTTP_409_CONFLICT,
+            detail="Shipment must be assigned a driver to start trip"
+        )
+    if shipment.driver_id is None:
+        raise HTTPException(
+            status_code=status.HTTP_409_CONFLICT,
+            detail="Driver not assigned to shipment"
+        )
+    driver = db.query(Driver).filter(Driver.id == shipment.driver_id).first()
+    if not driver:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Driver not found"
+        )
+    shipment.status = Status.IN_TRANSIT
+    driver.availability = Availability.ON_TRIP
+    db.commit()
+    db.refresh(shipment)
+    db.refresh(driver)
+
+    return shipment
+
+def end_transit(db: Session, shipment_id: int):
+    shipment = db.query(Shipment).filter(Shipment.id == shipment_id).first()
+    if not shipment:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Shipment not found"
+        )
+    if shipment.status == Status.PENDING or shipment.driver_id is None:
+        raise HTTPException(
+            status_code=status.HTTP_409_CONFLICT,
+            detail="Shipment has not beeen assigned a driver"
+        )
+    if shipment.status == Status.ASSIGNED:
+        raise HTTPException(
+            status_code=status.HTTP_409_CONFLICT,
+            detail="Shipment yet to begin transit"
+        )
+    if shipment.status == Status.COMPLETED:
+        raise HTTPException(
+            status_code=status.HTTP_409_CONFLICT,
+            detail="Shipment transport is already completed"
+        )
+    driver = db.query(Driver).filter(Driver.id == shipment.driver_id).first()
+    if not driver:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Driver not found"
+        )
+    if shipment.status == Status.IN_TRANSIT:
+        shipment.status = Status.COMPLETED
+        driver.availability = Availability.AVAILABLE
+        db.commit()
+        db.refresh(shipment)
+        db.refresh(driver)
+
+        return shipment
